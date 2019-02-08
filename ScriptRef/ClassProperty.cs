29a56fb7c90aaf40eb7b3d16030064ac87e2e07a
CMSQATeam <43958307+CMSQATeam@users.noreply.github.com>
2019-02-08 12:05:06 +0530

    Create ClassProperty.cs

---
+++ ScriptRef/ClassProperty.cs
@@ -0,0 +1,11 @@
+namespace DiffPlex.DiffBuilder.Model
+{
+    public class ClassProperty
+    {
+        public int NumberA { get; set; }
+
+        public int NumberB { get; set; }
+
+        public int NumberC { get; set; }
+    }
+}
